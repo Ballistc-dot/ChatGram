fc25e2d82273c89a52b0f2dc48d8073d4cfb239b
Pedro Vitor <pedrocdn2014@gmail.com>
2023-11-02 01:47:24 -0300

    removing husky install

--- packages/server/lib/redis.ts
+++ packages/server/lib/redis.ts
@@ -4,7 +4,13 @@
 import Redis from 'ioredis'
 import { promisify } from 'util'
 
-const redisClient = new Redis()
+const redisClient = new new Redis({
+  port: 6379, // Redis port
+  host: '127.0.0.1', // Redis host
+  username: 'default', // needs Redis >= 6
+  password: 'my-top-secret',
+  db: 0, // Defaults to 0
+})()
 
 function getRedis(value: string) {
   const syncRedisGet = promisify(redisClient.get).bind(redisClient)
